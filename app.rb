6231dbf074d1cb79aed28c361dd5021dbacc8d3f
Tammy Metz <tammy@tammymetz.com>
2018-11-12 23:52:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,13 +14,13 @@ module FormsLab
 
     post "/pirates" do
       @pirate = Pirate.new(params[:pirate])
- 
+
       params[:pirate][:ships].each do |ship_info|
         Ship.new(ship_info)
       end
- 
+
       @ships = Ship.all
- 
+
       erb :show
     end
   end
