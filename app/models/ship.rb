ea29666b9cee154ae0ccc11e741aec13db1e50e8
Tammy Metz <tammy@tammymetz.com>
2018-11-13 00:05:00 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+  @@all = []
+  attr_accessor :name, :type, :booty
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
